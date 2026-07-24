42cf14fda89d6d91ae8a9f5a1983c7ffbc793b8d
Dan Walmsley <4672627+danwalmsley@users.noreply.github.com>
2026-07-24 16:41:10 +0100

    Build Linux V8 in Ubuntu 22.04

--- scripts/build-native-engine-runtime.ps1
+++ scripts/build-native-engine-runtime.ps1
@@ -166,7 +166,7 @@ try {
     if ($LASTEXITCODE -ne 0) { throw "Failed to add the native runtime package to a consumer." }
     & dotnet restore $consumerProject -r $Rid `
         --source $Output `
-        --source https://api.nuget.org/v3/index.json
+        --source "https://api.nuget.org/v3/index.json"
     if ($LASTEXITCODE -ne 0) { throw "Failed to restore the native runtime package into a consumer." }
     & dotnet build $consumerProject -c Release -r $Rid --no-restore
     if ($LASTEXITCODE -ne 0) { throw "Failed to build the native runtime package consumer." }
