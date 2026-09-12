97f9b926e6d3c92fc2e4a5a0bab48fc05e914d88
Dan Walmsley <4672627+danwalmsley@users.noreply.github.com>
2026-09-12 12:21:28 +0100

    Enforce typed CSS declaration construction

--- experiments/WebScene.NativeEngine.Probe/native/webscene_css_state.h
+++ experiments/WebScene.NativeEngine.Probe/native/webscene_css_state.h
@@ -6,6 +6,7 @@
 #include <string_view>
 #include <unordered_map>
 #include <unordered_set>
+#include <utility>
 #include <vector>
 
 namespace webscene_native::css {
@@ -17,6 +18,14 @@ namespace webscene_native::css {
         bool important{false};
         css_property_id property{css_property_id::unknown};
         specified_css_value specified{};
+
+        css_declaration() = default;
+        css_declaration(std::string name_value, std::string value_value, bool important_value=false)
+            : name(std::move(name_value)), value(std::move(value_value)), important(important_value)
+        {
+            property = property_id(name);
+            specified = compile_specified_value(property, value);
+        }
 
         bool has_typed_value() const noexcept { return specified.fully_typed(); }
     };
